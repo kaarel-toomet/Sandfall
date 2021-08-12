37165320765bbc4afb6fd86031947b63b8551d59
aloaloolaola <alo.virunurm@gmail.com>
2021-08-13 00:30:13 +0300

    added more things and made fixes
    There are now liquid saw and fire in different states. Powders can now fall through liquids and gases. Liquids fall through gases. The end of the indicator for line placement snaps to the middle of a square.

--- materials.py
+++ materials.py
@@ -14,10 +14,18 @@ class Material:
     
     def update(self, cells, dimx, dimy):
         return
+
+class Empty:
+    state = ""
+
+class Full:
+    state = "1"
         
 class Powder(Material):
+    state = "powder"
+    
     def update(self, cells, dimx, dimy):
-        self.move(cells, dimy, dimx)
+        self.move(cells, dimx, dimy)
     
     def move(self, cells, dimx, dimy):
         x, y = self.x, self.y
@@ -26,7 +34,8 @@ class Powder(Material):
         cell_3 = get_cell(x+1, y+1, cells, dimx, dimy)      #123
         cell_4 = get_cell(x-1, y, cells, dimx, dimy)
         cell_6 = get_cell(x+1, y, cells, dimx, dimy)
-        empty = [False, cell_1 == 0, cell_2 == 0, cell_3 == 0, cell_4 == 0, False, cell_6 == 0]
+        empty = [False, cell_1.state == "", cell_2.state == "", cell_3.state == "", cell_4.state == "", False, cell_6.state == ""]
+        fluid = [False, cell_1.state in ("liquid", "gas"), cell_2.state in ("liquid", "gas"), cell_3.state in ("liquid", "gas"), cell_4.state in ("liquid", "gas"), False, cell_6.state in ("liquid", "gas")]
 
         if empty[2]:
             self.y += 1
@@ -45,18 +54,52 @@ class Powder(Material):
             self.y += 1
             self.x += 1
 
-class Liquid(Material):
-    def update(self, cells, dimx, dimy):
-        self.move(cells, dimy, dimx)
+        elif fluid[2]:
+            cell_2.y -= 1
+            self.y += 1
 
-    def move(self, cells, dimy, dimx):
+        elif fluid[1] and fluid[3] and fluid[4] and fluid[6]:
+            self.y += 1
+            if randint(0, 1) == 0:
+                cell_1.x += 1
+                cell_1.y -= 1
+                self.x -= 1
+            else:
+                cell_3.x -= 1
+                cell_3.y -= 1
+                self.x += 1
+        elif fluid[1] and fluid[4]:
+            cell_1.x += 1
+            cell_1.y -= 1
+            self.y += 1
+            self.x -= 1
+                
+        elif fluid[3] and fluid[6]:
+            cell_3.x -= 1
+            cell_3.y -= 1
+            self.y += 1
+            self.x += 1
+        
+
+class Solid(Material):
+    state = "solid"
+    pass
+
+class Liquid(Material):
+    state = "liquid"
+    
+    def update(self, cells, dimx, dimy):
+        self.move(cells, dimx, dimy)
+
+    def move(self, cells, dimx, dimy):
         x, y = self.x, self.y
         cell_1 = get_cell(x-1, y+1, cells, dimx, dimy)      #789
         cell_2 = get_cell(x, y+1, cells, dimx, dimy)        #4 6
         cell_3 = get_cell(x+1, y+1, cells, dimx, dimy)      #123
         cell_4 = get_cell(x-1, y, cells, dimx, dimy)
         cell_6 = get_cell(x+1, y, cells, dimx, dimy)
-        empty = [False, cell_1 == 0, cell_2 == 0, cell_3 == 0, cell_4 == 0, False, cell_6 == 0]
+        empty = [False, cell_1.state == "", cell_2.state == "", cell_3.state == "", cell_4.state == "", False, cell_6.state == ""]
+        gas = [False, cell_1.state == "gas", cell_2.state == "gas", cell_3.state == "gas", cell_4.state == "gas", False, cell_6.state == "gas"]
 
         if empty[2]:
             self.y += 1
@@ -87,18 +130,46 @@ class Liquid(Material):
         elif empty[6]:
             self.x += 1
 
-class Gas(Material):
-    def update(self, cells, dimx, dimy):
-        self.move(cells, dimy, dimx)
+        elif gas[2]:
+            cell_2.y -= 1
+            self.y += 1
 
-    def move(self, cells, dimy, dimx):
+        elif gas[1] and gas[3] and gas[4] and gas[6]:
+            self.y += 1
+            if randint(0, 1) == 0:
+                cell_1.x += 1
+                cell_1.y -= 1
+                self.x -= 1
+            else:
+                cell_3.x -= 1
+                cell_3.y -= 1
+                self.x += 1
+        elif gas[1] and gas[4]:
+            cell_1.x += 1
+            cell_1.y -= 1
+            self.y += 1
+            self.x -= 1
+                
+        elif gas[3] and gas[6]:
+            cell_3.x -= 1
+            cell_3.y -= 1
+            self.y += 1
+            self.x += 1
+
+class Gas(Material):
+    state = "gas"
+    
+    def update(self, cells, dimx, dimy):
+        self.move(cells, dimx, dimy)
+
+    def move(self, cells, dimx, dimy):
         x, y = self.x, self.y
         cell_7 = get_cell(x-1, y-1, cells, dimx, dimy)      #789
         cell_8 = get_cell(x, y-1, cells, dimx, dimy)        #4 6
         cell_9 = get_cell(x+1, y-1, cells, dimx, dimy)      #123
         cell_4 = get_cell(x-1, y, cells, dimx, dimy)
         cell_6 = get_cell(x+1, y, cells, dimx, dimy)
-        empty = [False, False, False, False, cell_4 == 0, False, cell_6 == 0, cell_7 == 0, cell_8 == 0, cell_9 == 0]
+        empty = [False, False, False, False, cell_4.state == "", False, cell_6.state == "", cell_7.state == "", cell_8.state == "", cell_9.state == ""]
 
         if empty[8]:
             self.y -= 1
@@ -129,66 +200,7 @@ class Gas(Material):
         elif empty[6]:
             self.x += 1
 
-class Solid(Material):
-    
-    def update(self, cells, dimx, dimy):
-        return
-
-
-class Sand(Powder):
-    name = "sand"
-    color = (200, 200, 150)
-    flammability = 0
-    
-class Sawdust(Powder):
-    name = "sawdust"
-    color = (255, 255, 200)
-    flammability = 7
-
-class Wall(Solid):
-    name = "wood"
-    color = (100, 100, 100)
-    flammability = 0
-
-class Saw(Solid):
-    name = "saw"
-    color = (150, 150, 150)
-    flammability = 1
-
-class Water(Liquid):
-    name = "water"
-    color = (50, 100, 200)
-    flammability = 0
-
-class SawGas(Gas):
-    name = "sawgas"
-    color = (0, 100, 0)
-    flammability = 20
-
-class Steam(Gas):
-    name = "Steam"
-    color = (100, 150, 255)
-    flammability = 0
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.life = randint(-200, -100)
-
-    def update(self, cells, dimx, dimy):
-        self.condensate(cells, dimx, dimy)
-        self.move(cells, dimy, dimx)
-
-    def condensate(self, cells, dimx, dimy):
-        self.life += 1
-        if 0 < self.life:
-            cells.remove(get_cell(self.x, self.y, cells, dimx, dimy))
-            cells.append(Water(self.x, self.y))
-
-class Fire(Solid):
-    name = "fire"
-    color = (255, 100, 0)
-    flammability = 0
+class Fire(Material):
 
     def __init__(self, x, y):
         self.x = x
@@ -197,7 +209,7 @@ class Fire(Solid):
 
     def update(self, cells, dimx, dimy):
         self.extinguish(cells, dimx, dimy)
-        self.spread(cells, dimy, dimx)
+        self.spread(cells, dimx, dimy)
 
     def extinguish(self, cells, dimx, dimy):
         self.life += 1
@@ -211,22 +223,127 @@ class Fire(Solid):
                 cells.append(Steam(cell.x, cell.y))
             elif randint(0, 100) < cell.flammability:
                 cells.remove(cell)
-                fire = Fire(cell.x, cell.y)
-                fire.life = -100 / cell.flammability
+                if cell.state == "powder":
+                    fire_type = PowderFire
+                elif cell.state == "solid":
+                    fire_type = SolidFire
+                elif cell.state == "liquid":
+                    fire_type = LiquidFire
+                elif cell.state == "gas":
+                    fire_type = GasFire
+                fire = fire_type(cell.x, cell.y)
+                fire.life = cell.burn_time * -1
                 cells.append(fire)
-                
-                
+
+
+class Sand(Powder):
+    name = "sand"
+    color = (200, 200, 150)
+    flammability = 0
+    
+class SawDust(Powder):
+    name = "sawdust"
+    color = (255, 255, 200)
+    flammability = 10
+    burn_time = 20
+
+class Wall(Solid):
+    name = "wood"
+    color = (100, 100, 100)
+    flammability = 0
+
+class Saw(Solid):
+    name = "saw"
+    color = (150, 150, 150)
+    flammability = 1
+    burn_time = 100
+
+class Water(Liquid):
+    name = "water"
+    color = (50, 100, 200)
+    flammability = 0
+
+class SawLiquid(Liquid):
+    name = "sawliquid"
+    color = (50, 100, 75)
+    flammability = 15
+    burn_time = 200
+
+class SawGas(Gas):
+    name = "sawgas"
+    color = (0, 100, 0)
+    flammability = 20
+    burn_time = 5
+
+class Steam(Gas):
+    name = "Steam"
+    color = (100, 150, 255)
+    flammability = 0
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.life = randint(-200, -100)
+
+    def update(self, cells, dimx, dimy):
+        self.condensate(cells, dimx, dimy)
+        self.move(cells, dimx, dimy)
+
+    def condensate(self, cells, dimx, dimy):
+        self.life += 1
+        if 0 < self.life:
+            cells.remove(get_cell(self.x, self.y, cells, dimx, dimy))
+            cells.append(Water(self.x, self.y))
+
+class PowderFire(Fire, Powder):
+    name = "fire"
+    color = (255, 100, 0)
+    flammability = 0
+
+    def update(self, cells, dimx, dimy):
+        self.extinguish(cells, dimx, dimy)
+        self.spread(cells, dimx, dimy)
+        self.move(cells, dimx, dimy)
+
+class SolidFire(Fire, Solid):
+    name = "fire"
+    color = (255, 100, 0)
+    flammability = 0
+
+class LiquidFire(Fire, Liquid):
+    name = "fire"
+    color = (255, 100, 0)
+    flammability = 0
+
+    def update(self, cells, dimx, dimy):
+        self.extinguish(cells, dimx, dimy)
+        self.spread(cells, dimx, dimy)
+        self.move(cells, dimx, dimy)
+
+class GasFire(Fire, Gas):
+    name = "fire"
+    color = (255, 100, 0)
+    flammability = 0
+
+    def update(self, cells, dimx, dimy):
+        self.extinguish(cells, dimx, dimy)
+        self.spread(cells, dimx, dimy)
+        self.move(cells, dimx, dimy)
 
 
 material_dict = {
 "0":Sand,
-"1":Sawdust,
-"2":Wall,
-"3":Saw,
-"4":Water,
-"5":Steam,
-"6":SawGas,
-"7":Fire
+"1":Wall,
+"2":Water,
+"3":Steam,
+"4":SawDust,
+"5":Saw,
+"6":SawLiquid,
+"7":SawGas,
+"8":PowderFire,
+"9":SolidFire,
+"10":LiquidFire,
+"11":GasFire
     }
 
 num_materials = len(material_dict)
@@ -238,8 +355,8 @@ def get_cell(x, y, cells, dimx, dimy):
             if cell.x == x and cell.y == y:
                 return cell
     else:
-        return 1
-    return 0
+        return Full
+    return Empty
 
 def get_nearby_cells(x, y, cells):
     results = []
